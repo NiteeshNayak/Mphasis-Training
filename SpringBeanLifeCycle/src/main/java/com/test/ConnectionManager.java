97a563a7ed9b43e912a4053a27f614f60dcb09e5
Niteesh X <niteesh.x@mphasis.com>
2026-02-24 16:51:02 +0530

    Life cycle of spring bean

---
+++ SpringBeanLifeCycle/src/main/java/com/test/ConnectionManager.java
@@ -0,0 +1,37 @@
+package com.test;
+
+import org.springframework.stereotype.Component;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
+@Component
+public class ConnectionManager {
+	private boolean isConnected = false;
+	
+	@PostConstruct
+	public void connect() {
+		System.out.println("Connecting to external service");
+		
+		isConnected = true;
+		
+		System.out.println("Connection established isConnected : "+isConnected);
+	}
+	
+	@PreDestroy
+	public void disconnect() {
+		System.out.println("Disconnecting from external service..");
+		
+		isConnected = false;
+		
+		System.out.println("Connection closed isConnected : "+isConnected);
+	}
+	
+	public void operation() {
+		if(isConnected) {
+			System.out.println("Performing Operation with active service");
+		}else {
+			System.out.println("Operation failed not connecting to external service");
+		}
+	}
+}
