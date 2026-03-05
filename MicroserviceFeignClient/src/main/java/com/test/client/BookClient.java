15d6833559e989d3e0adde1765311f17f421edd3
Niteesh X <niteesh.x@mphasis.com>
2026-03-05 09:32:25 +0530

    Adding Spring Microservices using Feign for project SpringBootRestApi

---
+++ MicroserviceFeignClient/src/main/java/com/test/client/BookClient.java
@@ -0,0 +1,20 @@
+package com.test.client;
+
+import java.util.List;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.test.entity.Book;
+
+@FeignClient(
+			name = "SpringBootRestAPI",
+			url = "http://localhost:3737"
+		)
+public interface BookClient {
+	
+	@GetMapping("/list")
+	public List<Book> listAllBooks();
+	
+	
+}
